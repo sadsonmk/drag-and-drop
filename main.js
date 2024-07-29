b48638f57ba7969d4489982facfc9031b4a69dea
iamsadson <sadiemcmirror@gmail.com>
2024-07-29 14:02:34 +0800

    Adding initial files to the remote repository, the html, css and js

---
+++ main.js
@@ -0,0 +1,31 @@
+// We use three event handlers here:
+
+// in the dragstart event handler, we get a reference to the element that the user dragged
+// in the dragover event handler for the target container, we call event.preventDefault(), which enables it to receive drop events.
+// in the drop event handler for the drop zone, we handle moving the draggable element from the original container to the drop zone.
+
+
+const items = document.querySelectorAll('.item');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+items.forEach(item => {
+    item.addEventListener('dragstart', function (ev) {
+        ev.dataTransfer.setData('text/plain', ev.target.id);
+    })
+})
+const sections = [left, right];
+sections.forEach(section => {
+    section.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    })
+
+    section.addEventListener('drop', function (e) {
+        e.preventDefault();
+        const id = e.dataTransfer.getData('text/plain');
+       const elem = document.getElementById(id);
+       section.appendChild(elem);
+    })
+})
+
+
