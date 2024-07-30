104044a7eebf9bca1ada1241823c425414966c67
iamsadson <sadiemcmirror@gmail.com>
2024-07-30 15:10:31 +0800

    changes in the main.js, put some comments on each main logic

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-// We use three event handlers here:
-
-// in the dragstart event handler, we get a reference to the element that the user dragged
-// in the dragover event handler for the target container, we call event.preventDefault(), which enables it to receive drop events.
-// in the drop event handler for the drop zone, we handle moving the draggable element from the original container to the drop zone.
-
-
+// draggable and droppable elements
 const items = document.querySelectorAll('.item');
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
-
+// We use three event handlers here:
+// in the dragstart event handler, we get a reference to the element that the user dragged
 items.forEach(item => {
-    item.addEventListener('dragstart', function (ev) {
-        ev.dataTransfer.setData('text/plain', ev.target.id);
-    })
+    item.addEventListener('dragstart', function(e) {
+        e.dataTransfer.setData('text/plain', e.target.id);
+    } )
 })
+
+// in the dragover event handler for the target container, we call event.preventDefault(), which enables it to receive drop events.
 const sections = [left, right];
 sections.forEach(section => {
-    section.addEventListener('dragover', function (e) {
+    section.addEventListener('dragover', function(e) {
         e.preventDefault();
     })
 
-    section.addEventListener('drop', function (e) {
+    // in the drop event handler for the drop zone, we handle moving the draggable element from the original container to the drop zone.
+    section.addEventListener('drop', function(e) {
         e.preventDefault();
         const id = e.dataTransfer.getData('text/plain');
-       const elem = document.getElementById(id);
-       section.appendChild(elem);
+        const elem = document.getElementById(id);
+        section.appendChild(elem);
     })
 })
+
+
+
+
 
 
